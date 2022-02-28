c26b45d24b7a174ac5a9b6e16326f1a6425630ae
ColorQian <qjl@njust.edu.cn>
2022-02-28 15:20:31 +0800

    构造二叉树节点

---
+++ jianzhiOffer2/src/com/jianzhioffer2/binarytree/TreeNode.java
@@ -0,0 +1,14 @@
+package com.jianzhioffer2.binarytree;
+
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode() {}
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
